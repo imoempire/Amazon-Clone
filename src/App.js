66b664875e3334bbb3fae749b8d9778b49de5c0e
imoempire <mrimoempire@gmail.com>
2021-10-27 10:26:46 +0000

    adding routing and a store&firebase config/reduers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Checkout from "./Components/Checkout/Checkout";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Home/Login";
+import Register from "./Components/Home/Register";
+import { auth } from "./Firebase/firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        {/* <Header /> */}
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route> 
+          <Route exact path="/register">
+            <Register />
+          </Route> 
+
+          <Route exact path="/checkOut">
+          <Header />
+            <Checkout />
+          </Route>
+
+          <Route exact path="/">
+          <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
