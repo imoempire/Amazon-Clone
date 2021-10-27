ba107e3ccefbf5c299cd45b056463f819ec56ba1
imoempire <mrimoempire@gmail.com>
2021-10-27 14:15:30 +0000

    change product info

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,11 +13,12 @@ function Home() {
         />
         <div className="home__row">
         <Product
-            title="The Lean Pack"
-            image="https://www.inquasar.com/wp-content/uploads/2016/11/the-lean-startup.png"
-            price={29.99}
-            rating={5}         
-         />
+            id="12321341"
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
           <Product
             id="49538094"
             title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
